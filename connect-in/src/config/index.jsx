6456459ee9a5865993168e6770074c66f611d164
Shaik Dawood <4dawoodshaik@gmail.com>
2026-04-15 12:34:13 +0530

    frontend deployment

--- connect-in/src/config/index.jsx
+++ connect-in/src/config/index.jsx
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 
-export const BASE_URL = "https://connectin-k515.onrender.com"
+export const BASE_URL = "https://connectin-zps8.onrender.com"
 
 export const clientServer = axios.create({
     baseURL: BASE_URL
